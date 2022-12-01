6157ec5f84f7eaf76914d56f35c2e115e5b8258f
okantasin <okan.tasin@hotmail.com>
2022-12-01 14:00:52 +0300

    Started e-commerce project.

---
+++ ecommer-demo-pair6/src/main/java/com/etiya/ecommerdemopair6/EcommerDemoPair6Application.java
@@ -0,0 +1,13 @@
+package com.etiya.ecommerdemopair6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerDemoPair6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerDemoPair6Application.class, args);
+	}
+
+}
---
+++ ecommer-demo-pair6/src/test/java/com/etiya/ecommerdemopair6/EcommerDemoPair6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etiya.ecommerdemopair6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerDemoPair6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
